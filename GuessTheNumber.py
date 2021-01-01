2c66922d7c8d856a85e6a415101693fe973d0066
AhonBandyopadhyay <57537678+AhonBandyopadhyay@users.noreply.github.com>
2021-01-02 00:13:02 +0530

    Update GuessTheNumber.py

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -4,7 +4,7 @@ print('Hello, What is your name?')
 name = input()
 secretNumber = random.randint(1, 20)
 print('Well, ' + name + ',  I am thinking of a number between 1 and 20.')
-print('You have five trys, Use then wisely....')
+print('You have five trys, Use them wisely....')
 
 # Ask the player to guess 5 times.
 for guessesTaken in range(1,6):
@@ -17,6 +17,6 @@ for guessesTaken in range(1,6):
     else:
         break    #  This condition is the correct guess!!
 if guess == secretNumber:
-    print('Good job, ' + name + '!  You guess my number correctly.')
+    print('Good job, ' + name + '!  You guessed my number correctly.')
 else:
     print('Nope. The number I was thinking of was  ' +  str(secretNumber) + '.')
